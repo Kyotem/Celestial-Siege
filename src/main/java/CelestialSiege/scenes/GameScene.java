0c8536b538c66c3a72acee58e15704432dee139f
Quinn Schwachöfer <Q.schwachofer@student.han.nl>
2024-03-22 13:46:14 +0100

    Aangepaste develop

---
+++ src/main/java/CelestialSiege/scenes/GameScene.java
@@ -0,0 +1,16 @@
+package CelestialSiege.scenes;
+
+import com.github.hanyaeger.api.scenes.DynamicScene;
+
+public class GameScene extends DynamicScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
+    //Test Quinn
+}
