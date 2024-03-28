6f5ba3377d38d3f9cda9075af2e515a712de4cd8
Finn Panhuijsen <FB.Panhuijsen@student.han.nl>
2024-03-28 20:53:16 +0100

    * Refactored code & added comments to PauseButton
    * Added the functionality to change the text of the button between pausestates

--- src/main/java/CelestialSiege/buttons/PauseButton.java
+++ src/main/java/CelestialSiege/buttons/PauseButton.java
@@ -14,17 +14,29 @@ public class PauseButton extends Button implements MouseButtonPressedListener {
         this.gameScene = gameScene;
     }
 
+    // Method called when button is pressed -> toggles pause state & buttontext
     @Override
     public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
+        updateButtonText();
         gameScene.pauseResumeGWU();
+
     }
 
-
+    // Toggle the game state between paused and resumed
     private void pauseResumeGWU() {
         if (gameScene.isActiveGWU()) {
             gameScene.pause();
         } else {
             gameScene.resume();
+        }
+    }
+
+    // Update the button text based on the current game state
+    private void updateButtonText() {
+        if (gameScene.isActiveGWU()) {
+            setText("Resume");
+        } else {
+            setText("Pause");
         }
     }
 }
