e8cb84be66ea7c4eac1bf73abcfe4a7e8870bb2d
Finn Panhuijsen <FB.Panhuijsen@student.han.nl>
2024-03-28 18:23:26 +0100

    Added comments to Bullet & BulletSpawner

--- src/main/java/CelestialSiege/entities/BulletSpawner.java
+++ src/main/java/CelestialSiege/entities/BulletSpawner.java
@@ -4,17 +4,13 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.EntitySpawner;
 
 import java.util.Random;
-
 public class BulletSpawner extends EntitySpawner {
 
-
     // TODO Resolve height spawn issue (Base on compentity / aliens?)
-
     private final Spaceship spaceship;
     private final AlienManager alienManager;
     private final Random random;
     private int alienBulletSpawnCounter = 1; // Used to determine @ which interval the aliens should be able to shoot a bullet
-
     public BulletSpawner(long intervalInMs, Spaceship spaceship, AlienManager alienManager) {
         super(intervalInMs);
         this.spaceship = spaceship;
